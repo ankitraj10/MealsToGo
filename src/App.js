bf91730c64a57194e5f715732501d7ea43cad62d
ankitraj10 <ankitraj.mail1@gmail.com>
2025-01-16 23:56:48 +0530

    star and open SVG added , code cleanup

--- src/App.js
+++ src/App.js
@@ -3,8 +3,20 @@ import { View, Text, SafeAreaView, StatusBar, Platform, StyleSheet } from 'react
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import { ReastaurantsScreen } from "./features/reastaurant/screens/reastaurants.screens";
 import { ThemeProvider } from "styled-components/native";
-import { theme } from "./infrastructure/theme"
+import { theme } from "./infrastructure/theme";
+import { useFonts as useOswald, Oswald_400Regular } from '@expo-google-fonts/oswald';
+import { useFonts as useLato, Lato_400Regular } from '@expo-google-fonts/lato';
 const App = () => {
+    let [oswaldLoded] = useOswald({
+        Oswald_400Regular,
+    });
+    let [latoLoded] = useLato({
+        Lato_400Regular,
+    });
+
+    if (!oswaldLoded || !latoLoded) {
+        return null;
+    }
     return (
         <>
             <ThemeProvider theme={theme}>
